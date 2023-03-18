822ee7056b3e3f7a0ed9adde1a1e2af28f275a82
Jishnu Raj <jishnurajkarockal2001@gmail.com>
2023-03-18 20:26:07 +0530

    almost done with the things again

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Haii ,Welcome to SERVER1");
+})
+
+app.listen(4500,()=>{
+    console.log("server is running at port 4500");
+})
